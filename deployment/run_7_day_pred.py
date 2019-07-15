792762ed26cff12a585645cc663e88da5c764c28
Peng Wang <39747935+wangpengcn@users.noreply.github.com>
2019-07-14 21:36:50 -0700

    Update run_7_day_pred.py

--- deployment/run_7_day_pred.py
+++ deployment/run_7_day_pred.py
@@ -12,24 +12,21 @@ from wtforms.fields.html5 import DateField
 from wtforms_components import DateRange
 from flask_wtf import Form
 from flask_bootstrap import Bootstrap
-import json
 
-FILE_CONFIG = r'../config.json'
-# Load config variables    
-with open(FILE_CONFIG) as json_config_file:
-        config = json.load(json_config_file)
-        
 app = Flask(__name__)
-app.config['SECRET_KEY'] = config["key"]["SECRET_KEY"]
+app.config['SECRET_KEY'] = 'border-prediction'
 Bootstrap(app)
+FILE_PREDICTION = r'../results/7_day_forecast.csv'
 
-df_preds = pd.read_csv(config["file"]["FILE_PREDICTION"]) 
+df_preds = pd.read_csv(FILE_PREDICTION) 
 df_preds['Date Time'] = pd.to_datetime(df_preds['Date Time'])
 df_preds['Date'] = df_preds['Date Time'].dt.date
+pred_img = './static/7_day_prediction.png'
+DAYS = 7
 
 class DatePicker(Form):
     date_max = df_preds['Date'].max()
-    date_min = date_max - timedelta(days=config["number"]["FORECAST_DAYS"]-1)
+    date_min = date_max - timedelta(days=DAYS-1)
     min_date = DateField(id='date_min_field',default=date_min)
     max_date = DateField(id='date_max_field',default=date_max)
     date_selected = DateField(id='datepick',default=date_max, validators=[DateRange(min=date_min, max=date_max)])
@@ -37,13 +34,14 @@ class DatePicker(Form):
 @app.route('/', methods=['GET','POST'])
 def index(): 
     form = DatePicker(id="myform")        
-    return render_template('index.html', form=form, pred_image = config["file"]["OUTPUT_IMG_PREDICTION"])
+    return render_template('index.html', form=form, pred_image = pred_img)
 
 @app.route('/predictions')
 def predictions():       
     date_selected = request.args.get('jsdata')
     df_pred_selected = df_preds[df_preds['Date']==datetime.strptime(date_selected,'%Y-%m-%d').date()]
     return render_template('predictions.html',tables=[df_pred_selected[['Date Time','Expected Delay in Minutes']].to_html(classes='data',index=False,header=False)], titles=df_pred_selected.columns.values)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
