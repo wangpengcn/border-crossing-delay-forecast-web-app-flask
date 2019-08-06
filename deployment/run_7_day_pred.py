940022ccb7b33b957b5eb1db8e6dd4055619ed97
Peng Wang <39747935+wangpengcn@users.noreply.github.com>
2019-08-05 22:35:19 -0700

    update

--- deployment/run_7_day_pred.py
+++ deployment/run_7_day_pred.py
@@ -5,42 +5,62 @@ Created on Sat Jul  6 15:50:02 2019
 @author: wangp
 """
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from wtforms.fields.html5 import DateField
 from wtforms_components import DateRange
 from flask_wtf import Form
 from flask_bootstrap import Bootstrap
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'border-prediction'
 Bootstrap(app)
-FILE_PREDICTION = r'./results/7_day_forecast.csv'
-FILE_RMSE = r'./results/rmse_results.csv'
 
-df_preds = pd.read_csv(FILE_PREDICTION) 
-df_preds['Date_time'] = pd.to_datetime(df_preds['Date_time'])
-df_preds['Date'] = df_preds['Date_time'].dt.date
-df_rmse = pd.read_csv(FILE_RMSE) 
-pred_img = './static/7_day_prediction.png'
+FILE_CONFIG = r'./config.json'
 DAYS = 7
 
-class DatePicker(Form):
-    date_max = df_preds['Date'].max()
-    date_min = date_max - timedelta(days=DAYS-1)
-    min_date = DateField(id='date_min_field',default=date_min)
-    max_date = DateField(id='date_max_field',default=date_max)
-    date_selected = DateField(id='datepick',default=date_max, validators=[DateRange(min=date_min, max=date_max)])
-        
+class DatePicker(Form): 
+    date_selected = DateField(id='datepick')   
+    def __init__(self, date_min, date_max, *args, **kwargs): 
+        super(DatePicker, self).__init__(*args, **kwargs)
+        self.date_min = date_min
+        self.date_max = date_max        
+    
+    #min_date = DateField(id='date_min_field',default=date_min)
+    #max_date = DateField(id='date_max_field',default=date_max)
+    #date_selected = DateField(id='datepick',default=date_max, validators=[DateRange(min=date_min, max=date_max)])
+    
+def read_json_file(filename):
+    with open(filename) as json_file:
+        obj = json.load(json_file)
+    return obj
+
+# Load config variables    
+config = read_json_file(FILE_CONFIG)
+
 @app.route('/', methods=['GET','POST'])
 def index(): 
-    form = DatePicker(id="myform")        
+    df_preds = pd.read_csv(config["file"]["FILE_PREDICTION"]) 
+    df_preds['Date_time'] = pd.to_datetime(df_preds['Date_time'])
+    df_preds['Date'] = df_preds['Date_time'].dt.date
+    session["df_preds"] = df_preds.to_json()
+    df_rmse = pd.read_csv(config["file"]["RMSE_RESULTS"])
+    pred_img = './static/7_day_prediction.png'     
+    date_max = df_preds['Date'].max()
+    date_min = date_max - timedelta(days=DAYS-1)
+    form = DatePicker(date_min,date_max)  
+    form.date_selected.default = date_max
+    form.date_selected.validators =[DateRange(min=date_min, max=date_max)]
+    form.process()
     # Only need to display the last 30 days prediction RMSE
     return render_template('index.html', form=form, tables=[df_rmse[:30].to_html(classes='rmse',index=False,header=True)], pred_image = pred_img)
 
 @app.route('/predictions')
 def predictions():       
+    df_preds=session.get('df_preds')
+    df_preds=pd.read_json(df_preds, dtype=False)
     date_selected = request.args.get('jsdata')
     df_pred_selected = df_preds[df_preds['Date']==datetime.strptime(date_selected,'%Y-%m-%d').date()]
     return render_template('predictions.html',tables=[df_pred_selected[['Date_time','Expected Delay in Minutes']].to_html(classes='data',index=False,header=False)], titles=df_pred_selected.columns.values)
---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#1. git clone https://github.com/wangpengcn/border-crossing-delay-forecast-web-app-flask.git border_forecast
+#2. cd border_forecast
+#3. ./run.sh
+# chmod 700 run.sh (read, write, and execute (run) the script -- but only your user)
+FOLDER_PATH="/home/ec2-user/border_forecast/"
+FOLDER_NAME="border_forecast"
+VENV_NAME="venv_border_forecast"
+
+#Perform a yum install update
+sudo yum update
+
+#List of all python3 packages available
+#sudo yum list | grep python3
+#Install Python3.6
+sudo yum install python36-pip
+
+# Install virtualenv using pip3
+pip3 install --user virtualenv
+
+# Create a virtual environment
+python3 -m venv $VENV_NAME
+
+# Activate virtual environment
+source $VENV_NAME/bin/activate
+
+#Install required python packages
+$VENV_NAME/bin/pip3 install -r requirements.txt
+
+# schedule 05:00 everyday to rebuild prediction model and generate forecast for next 7 days
+(crontab -l 2>/dev/null; echo "0 5 * * * cd $FOLDER_PATH && $VENV_NAME/bin/python3 border_wait_time_forecast.py > /tmp/border.log") | crontab -
+
+#run flask
+export FLASK_APP=./deployment/run_7_day_pred.py
+$VENV_NAME/bin/python3 -m flask run --host=0.0.0.0
